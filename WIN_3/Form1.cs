ac41df38d06666b8a0e17cf87dec1d3ae87b4912
Krzysztof Grabek <krzysztofgrabek@gmail.com>
2017-04-09 16:54:02 +0200

    Start

--- WIN_3/Form1.cs
+++ WIN_3/Form1.cs
@@ -16,5 +16,10 @@ namespace WIN_3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
